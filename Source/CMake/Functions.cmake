c750bbd45f906b8a965e0b66316320d229be3fac
jineyne <jineyne@gmail.com>
2019-11-28 21:01:48 +0900

    fix: Update

--- Source/CMake/Functions.cmake
+++ Source/CMake/Functions.cmake
@@ -1,9 +1,10 @@
+# TODO: copy lib to bin folder
 function(set_compile_options PROJECT)
-    set_target_properties(${PROJECT}
-        PROPERTIES
-        ARCHIVE_OUTPUT_DIRECTORY "${CMAKE_BINARY_DIR}/lib/${PROJECT}"
-        LIBRARY_OUTPUT_DIRECTORY "${CMAKE_BINARY_DIR}/lib/${PROJECT}"
-        RUNTIME_OUTPUT_DIRECTORY "${CMAKE_BINARY_DIR}/bin/${PROJECT}")
+    #    set_target_properties(${PROJECT}
+    #        PROPERTIES
+    #        ARCHIVE_OUTPUT_DIRECTORY "${CMAKE_BINARY_DIR}/lib/${PROJECT}"
+    #        LIBRARY_OUTPUT_DIRECTORY "${CMAKE_BINARY_DIR}/lib/${PROJECT}"
+    #        RUNTIME_OUTPUT_DIRECTORY "${CMAKE_BINARY_DIR}/bin/${PROJECT}")
 endfunction()
 
 function(link_cpf PROJECT)
