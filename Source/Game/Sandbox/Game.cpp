8e00031265c4b5ae6bae52b376e894af6bfbb9c9
jineyne <jineyne@gmail.com>
2019-12-02 21:09:59 +0900

    test: Update Sandbox to test scene

--- Source/Game/Sandbox/Game.cpp
+++ Source/Game/Sandbox/Game.cpp
@@ -1,7 +1,25 @@
 #include "Application.hpp"
+
+#include "Manager/SceneManager.hpp"
+#include "Scene/Scene.hpp"
+
 #include "Debug/Debug.hpp"
 
 using namespace cpf;
+
+class SandboxScene : public Scene {
+public:
+    SandboxScene() : Scene("SandboxScene") {}
+
+protected:
+    void onStartUp() override {
+        Debug::LogInfo("SandboxScene::StartUp");
+    }
+
+    void onShutDown() override {
+        Debug::LogInfo("SandboxScene::ShutDown");
+    }
+};
 
 int main() {
     ApplicationCreateInfo appCI{};
@@ -9,7 +27,11 @@ int main() {
     appCI.primaryWindowCreateInfo.height = 720;
     appCI.primaryWindowCreateInfo.title = "Sandbox";
     appCI.primaryWindowCreateInfo.fullscreen = false;
+
     Application::StartUp(appCI);
+
+    SceneManager::Instance().createScene<SandboxScene>();
+
     Application::Instance().runMainLoop();
     Application::ShutDown();
 
