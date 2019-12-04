f4dc3c4aadcd5d74db55295ee881867910357174
jineyne <jineyne@gmail.com>
2019-12-04 19:43:37 +0900

    feat: Update forward declaration

--- Source/Foundation/cpf.hpp
+++ Source/Foundation/cpf.hpp
@@ -10,10 +10,37 @@ namespace cpf {
     template<class T>
     using UPtr = std::unique_ptr<T>;
 
+    class DataStream;
+    class FileSystem;
+    class Path;
+
+    class ObjectManager;
+    class RenderWindowManager;
+    class SceneManager;
+
+    class Math;
+    class Matrix3;
+    class Matrix4;
+    class Quaternion;
+
+    class Buffer;
+    class GpuBuffer;
+    class IndexBuffer;
+    class RenderAPI;
+    class RenderTarget;
+    class RenderWindow;
+    class Shadow;
+    class UniformBuffer;
+    class VertexBuffer;
+
     class RenderTarget;
     using HRenderTarget = SPtr<RenderTarget>;
     class RenderWindow;
     using HRenderWindow = SPtr<RenderWindow>;
+
+    class Actor;
+    class Object;
+    class Scene;
 
     class Application;
 }
