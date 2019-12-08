e7989d324e132a9de87b40f2e7547299e3214600
jineyne <jineyne@gmail.com>
2019-12-09 05:56:23 +0900

    test

--- Source/Game/Sandbox/Game.cpp
+++ Source/Game/Sandbox/Game.cpp
@@ -1,11 +1,88 @@
 #include "Application.hpp"
 
 #include "Manager/SceneManager.hpp"
+
+#include "RenderAPI/IndexBuffer.hpp"
+#include "RenderAPI/RenderAPI.hpp"
+#include "RenderAPI/Shader.hpp"
+#include "RenderAPI/VertexBuffer.hpp"
+#include "RenderAPI/VertexDataDesc.hpp"
+#include "RenderAPI/VertexDeclaration.hpp"
+
+#include "Renderer/Renderable.hpp"
+
+#include "Scene/Component.hpp"
 #include "Scene/Scene.hpp"
 
 #include "Debug/Debug.hpp"
 
 using namespace cpf;
+
+class Sprite : public Component, Renderable {
+private:
+    VertexDeclaration *mVertexDeclaration;
+    VertexBuffer *mBuffer;
+    IndexBuffer *mIndexBuffer;
+    Shader *mShader;
+
+public:
+    Sprite(Actor *owner) : Component(owner, "Sprite"), Renderable() {}
+
+public:
+    void onStartUp() override {
+        ShaderCreateInfo shaderCI{};
+        shaderCI.vertexInfo.type = EGpuProgramType::Vertex;
+        shaderCI.vertexInfo.source = "#version 330 core\n" "layout (location = 0) in vec3 POSITION;\n" "void main(){\n" " gl_Position = vec4(POSITION.x, POSITION.y, POSITION.z, 1.0);\n }";
+        shaderCI.fragmentInfo.type = EGpuProgramType::Fragment;
+        shaderCI.fragmentInfo.source = "#version 330 core\n" "out vec4 FragColor;\n" "void main() {\n" "FragColor = vec4(1.0, 0.5, 0.2, 1.0);\n}";
+        mShader = Allocator::New<Shader>(shaderCI);
+
+        VertexDataDesc *vdd = Allocator::New<VertexDataDesc>();
+        vdd->addElement(VertexElementType::Float3, VertexElementSemantic::Position);
+        // vdd->addElement(VertexElementType::Float4, VertexElementSemantic::Color);
+        mVertexDeclaration = Allocator::New<VertexDeclaration>(vdd->createElements());
+
+        float vertices[] = {
+            0.5f,  0.5f, 0.0f,  // top right
+            0.5f, -0.5f, 0.0f,  // bottom right
+            -0.5f, -0.5f, 0.0f,  // bottom left
+            -0.5f,  0.5f, 0.0f   // top left 
+        };
+        mBuffer = Allocator::New<VertexBuffer>(vdd->getVertexStride(), 12, BufferUsage::Default);
+        mBuffer->initialize();
+        float *vbp = static_cast<float *>(mBuffer->map(0, sizeof(vertices)));
+        memcpy(vbp, vertices, sizeof(vertices));
+        mBuffer->unmap();
+
+        uint32_t indices[] = {
+            0, 1, 2,   // first triangle
+            2, 3, 0    // second triangle
+        };
+
+        mIndexBuffer = Allocator::New<IndexBuffer>(sizeof(uint32_t), 6, BufferUsage::Default);
+        mIndexBuffer->initialize();
+        uint32_t *ibp = static_cast<uint32_t *>(mIndexBuffer->map(0, sizeof(indices)));
+        memcpy(ibp, indices, sizeof(indices));
+        mIndexBuffer->unmap();
+    }
+
+    void onShutDown() override {
+        Allocator::Delete(mIndexBuffer);
+        Allocator::Delete(mBuffer);
+        Allocator::Delete(mVertexDeclaration);
+    }
+
+    void render() override {
+        RenderAPI &rapi = RenderAPI::Instance();
+
+        rapi.setVertexDeclaration(mVertexDeclaration);
+        rapi.setVertexBuffer(0, {mBuffer});
+        rapi.setIndexBuffer(mIndexBuffer);
+        rapi.setShader(mShader);
+
+        rapi.drawElements(0, mIndexBuffer->getCount(), 0, mBuffer->getCount(), 0);
+    }
+};
 
 class SandboxScene : public Scene {
 public:
@@ -13,7 +90,8 @@ public:
 
 protected:
     void onStartUp() override {
-        Debug::LogInfo("SandboxScene::StartUp");
+        Actor *rectangle = spawnActor<Actor>("rectangle");
+        rectangle->addComponenet<Sprite>();
     }
 
     void onShutDown() override {
